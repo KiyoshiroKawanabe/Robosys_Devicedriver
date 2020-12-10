ce2670fb1971482b5f69a9f5e7aa23c659db2ec4
Kiyoshiro Kawanabe <s19C1031UL@s.chibakoudai.jp>
2020-12-10 12:38:31 +0900

    add a file

--- myled/myled.c
+++ myled/myled.c
@@ -8,7 +8,7 @@
 
 #define num 3
 
-MODULE_AUTHOR("Kiyoshiro Kawanabe");
+MODULE_AUTHOR("Ryuichi Ueda and Kiyoshiro Kawanabe");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
@@ -75,67 +75,67 @@ static ssize_t led_write (struct file* filp, const char* buf, size_t count, loff
 
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
 {
-        int size = 0;
-        char sushi[] = {0xF0, 0x9F, 0x8D, 0xA3, 0x0A};
-        if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
-            printk( KERN_INFO "sushi : copy_to_user failed\n");
-        return -EFAULT;
-        }
-        size += sizeof(sushi);
-        return size;
+    int size = 0;
+    char sushi[] = {0xF0, 0x9F, 0x8D, 0xA3, 0x0A};
+    if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
+        printk( KERN_INFO "sushi : copy_to_user failed\n");
+    return -EFAULT;
+    }
+    size += sizeof(sushi);
+    return size;
 }
 
 static struct file_operations led_fops = {
-        .owner = THIS_MODULE,
-        .write = led_write,
-        .read = sushi_read
+    .owner = THIS_MODULE,
+    .write = led_write,
+    .read = sushi_read
 };
 
 static int __init init_mod(void)
 {
-        int retval, i;
-        retval = alloc_chrdev_region(&dev, 0, 1, "myled");
-        if(retval < 0){
-                printk(KERN_ERR "alloc_chrdev_region failed.\n");
-                return retval;
-        }
-        printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+    int retval, i;
+    retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+    if(retval < 0){
+        printk(KERN_ERR "alloc_chrdev_region failed.\n");
+        return retval;
+    }
+    printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-        cdev_init(&cdv, &led_fops);
-        retval = cdev_add(&cdv, dev, 1);
-        if(retval < 0){
-                printk(KERN_ERR "cdev_add faild. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
-                return retval;
-        }
+    cdev_init(&cdv, &led_fops);
+    retval = cdev_add(&cdv, dev, 1);
+    if(retval < 0){
+        printk(KERN_ERR "cdev_add faild. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
+        return retval;
+    }
 
-        cls = class_create(THIS_MODULE,"myled");
-        if(IS_ERR(cls)){
-                printk(KERN_ERR "class_create failed.");
-                return PTR_ERR(cls);
-        }
+    cls = class_create(THIS_MODULE,"myled");
+    if(IS_ERR(cls)){
+        printk(KERN_ERR "class_create failed.");
+        return PTR_ERR(cls);
+    }
 
-        device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
+    device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
 
-        gpio_base = ioremap_nocache(0xfe200000, 0xA0);
+    gpio_base = ioremap_nocache(0xfe200000, 0xA0);
 
-        for(i = 0; i < num; i++){
-          const u32 led = list[i];
-          const u32 index = led/10;
-          const u32 shift = (led%10) * 3;
-          const u32 mask = ~(0x7 << shift);
-          gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
-        }
+    for(i = 0; i < num; i++){
+        const u32 led = list[i];
+        const u32 index = led/10;
+        const u32 shift = (led%10) * 3;
+        const u32 mask = ~(0x7 << shift);
+        gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+    }
 
-        return 0;
+    return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
-        cdev_del(&cdv);
-        device_destroy(cls, dev);
-        class_destroy(cls);
-        unregister_chrdev_region(dev, 1);
-        printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+    cdev_del(&cdv);
+    device_destroy(cls, dev);
+    class_destroy(cls);
+    unregister_chrdev_region(dev, 1);
+    printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);
